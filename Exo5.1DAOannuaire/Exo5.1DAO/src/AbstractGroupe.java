44db13feb9982bb987ea4dfe43deee9202adbe02
Utilisateur <user@debian.arcahe.ovh>
2019-03-01 14:13:08 +0100

    d

---
+++ Exo5.1DAOannuaire/Exo5.1DAO/src/AbstractGroupe.java
@@ -0,0 +1,9 @@
+
+import java.io.Serializable;
+
+public abstract class AbstractGroupe implements Serializable {
+	public AbstractGroupe() {}
+	
+	public abstract void affichageProfondeur();
+	public abstract void affichageLargeur();
+}
