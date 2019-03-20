b6479ec84bfb142bdeb775aec4fca3dbdb73b781
Utilisateur <user@debian.arcahe.ovh>
2019-03-20 11:25:56 +0100

    d

---
+++ Exo5.1DAOannuaire/Exo5.1DAO/src/JDBC.java
@@ -0,0 +1,71 @@
+
+import java.sql.*;
+
+public class JDBC {
+	
+	   static final String JDBC = "com.mysql.jdbc.Driver";  
+	   static final String DB = "jdbc:mysql://localhost";
+
+	   static final String USER = "username";
+	   static final String PASS = "password";
+	   
+	   public static void main(String[] args) {
+	   Connection conn = null;
+	   Statement stmt = null;
+	   try{
+	      //STEP 2: Register JDBC driver
+	      Class.forName("com.mysql.jdbc.Driver");
+
+	      //STEP 3: Open a connection
+	      System.out.println("Connecting to database...");
+	      conn = DriverManager.getConnection(DB,USER,PASS);
+
+	      //STEP 4: Execute a query
+	      System.out.println("Création du statement.");
+	      stmt = conn.createStatement();
+	      String sql;
+	      sql = "SELECT id, first, last, age FROM Employees";
+	      ResultSet rs = stmt.executeQuery(sql);
+	      
+	      //STEP 5: Extract data from result set
+	      while(rs.next()){
+	         //Retrieve by column name
+	         int id  = rs.getInt("id");
+	         int age = rs.getInt("age");
+	         String first = rs.getString("first");
+	         String last = rs.getString("last");
+
+	         //Display values
+	         System.out.print("ID: " + id);
+	         System.out.print(", Age: " + age);
+	         System.out.print(", First: " + first);
+	         System.out.println(", Last: " + last);
+	      }
+	      //STEP 6: Clean-up environment
+	      rs.close();
+	      stmt.close();
+	      conn.close();
+	   }catch(SQLException se){
+	      //Handle errors for JDBC
+	      se.printStackTrace();
+	   }catch(Exception e){
+	      //Handle errors for Class.forName
+	      e.printStackTrace();
+	   }finally{
+	      //finally block used to close resources
+	      try{
+	         if(stmt!=null)
+	            stmt.close();
+	      }catch(SQLException se2){
+	      }// nothing we can do
+	      try{
+	         if(conn!=null)
+	            conn.close();
+	      }catch(SQLException se){
+	         se.printStackTrace();
+	      }//end finally try
+	   }
+	   
+	}
+}
+
